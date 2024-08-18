1ab07cb3abdfc5f0677f4b22688eaac337668ad1
goncharov.vv <goncharov.vv@esphere.ru>
2024-08-18 20:52:45 +0300

    new file:   echo-bot/CatBot.py 	new file:   echo-bot/HelloBot.py 	new file:   echo-bot/RandomBot.py 	new file:   echo-bot/bot.py

---
+++ echo-bot/CatBot.py
@@ -0,0 +1,21 @@
+import time
+from telegram import Bot
+
+
+chat_id = 123  # замените на свое значение, подробнее ниже
+bot = Bot("YOUR_BOT_TOKEN")
+
+
+def send_random_cat():
+    url = f'https://cataas.com/cat?t=${time.time()}'
+    bot.send_photo(chat_id, url)
+    # bot.send_message(chat_id, "привет!")
+
+
+def main() -> None:
+    send_random_cat()
+    print('Cat has been sent')
+
+
+if __name__ == "__main__":
+    main()
